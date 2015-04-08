d6a37de62711046e3da87aeddabc20cc77de81bd
Jihoon Yoon <dyngina@gmail.com>
2015-04-08 09:27:34 +0900

    Init

---
+++ BackCropper/Form1.Designer.cs
@@ -0,0 +1,217 @@
+﻿namespace BackCropper {
+	partial class Form1 {
+		/// <summary>
+		/// 필수 디자이너 변수입니다.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// 사용 중인 모든 리소스를 정리합니다.
+		/// </summary>
+		/// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+		protected override void Dispose(bool disposing) {
+			if (disposing && (components != null)) {
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form 디자이너에서 생성한 코드
+
+		/// <summary>
+		/// 디자이너 지원에 필요한 메서드입니다.
+		/// 이 메서드의 내용을 코드 편집기로 수정하지 마십시오.
+		/// </summary>
+		private void InitializeComponent() {
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+			this.pictureBox1 = new System.Windows.Forms.PictureBox();
+			this.button1 = new System.Windows.Forms.Button();
+			this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+			this.label5 = new System.Windows.Forms.Label();
+			this.label6 = new System.Windows.Forms.Label();
+			this.label7 = new System.Windows.Forms.Label();
+			this.textBox1 = new System.Windows.Forms.TextBox();
+			this.textBox2 = new System.Windows.Forms.TextBox();
+			this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+			this.groupBox1 = new System.Windows.Forms.GroupBox();
+			this.label1 = new System.Windows.Forms.Label();
+			this.button2 = new System.Windows.Forms.Button();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// pictureBox1
+			// 
+			this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+			this.pictureBox1.Cursor = System.Windows.Forms.Cursors.Arrow;
+			this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+			this.pictureBox1.Location = new System.Drawing.Point(10, 10);
+			this.pictureBox1.Name = "pictureBox1";
+			this.pictureBox1.Size = new System.Drawing.Size(800, 500);
+			this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+			this.pictureBox1.TabIndex = 0;
+			this.pictureBox1.TabStop = false;
+			this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+			this.pictureBox1.Paint += new System.Windows.Forms.PaintEventHandler(this.pictureBox1_Paint);
+			this.pictureBox1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseMove);
+			// 
+			// button1
+			// 
+			this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+			this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+			this.button1.Font = new System.Drawing.Font("맑은 고딕", 9F);
+			this.button1.Location = new System.Drawing.Point(820, 10);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(192, 30);
+			this.button1.TabIndex = 1;
+			this.button1.TabStop = false;
+			this.button1.Text = "불러오기";
+			this.button1.UseVisualStyleBackColor = true;
+			this.button1.Click += new System.EventHandler(this.button1_Click);
+			// 
+			// openFileDialog1
+			// 
+			this.openFileDialog1.Filter = "그림 파일|*.jpg; *.png";
+			// 
+			// label5
+			// 
+			this.label5.AutoSize = true;
+			this.label5.Font = new System.Drawing.Font("맑은 고딕", 9F);
+			this.label5.Location = new System.Drawing.Point(816, 448);
+			this.label5.Name = "label5";
+			this.label5.Size = new System.Drawing.Size(43, 15);
+			this.label5.TabIndex = 6;
+			this.label5.Text = "해상도";
+			// 
+			// label6
+			// 
+			this.label6.AutoSize = true;
+			this.label6.Font = new System.Drawing.Font("맑은 고딕", 9F);
+			this.label6.Location = new System.Drawing.Point(816, 470);
+			this.label6.Name = "label6";
+			this.label6.Size = new System.Drawing.Size(31, 15);
+			this.label6.TabIndex = 7;
+			this.label6.Text = "가로";
+			// 
+			// label7
+			// 
+			this.label7.AutoSize = true;
+			this.label7.Font = new System.Drawing.Font("맑은 고딕", 9F);
+			this.label7.Location = new System.Drawing.Point(912, 470);
+			this.label7.Name = "label7";
+			this.label7.Size = new System.Drawing.Size(31, 15);
+			this.label7.TabIndex = 8;
+			this.label7.Text = "세로";
+			// 
+			// textBox1
+			// 
+			this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+			this.textBox1.Font = new System.Drawing.Font("맑은 고딕", 9F);
+			this.textBox1.Location = new System.Drawing.Point(818, 488);
+			this.textBox1.MaxLength = 4;
+			this.textBox1.Name = "textBox1";
+			this.textBox1.Size = new System.Drawing.Size(77, 23);
+			this.textBox1.TabIndex = 9;
+			this.textBox1.Text = "100";
+			this.textBox1.TextChanged += new System.EventHandler(this.textBox_TextChanged);
+			this.textBox1.KeyDown += new System.Windows.Forms.KeyEventHandler(this.textBox1_KeyDown);
+			this.textBox1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox1_KeyPress);
+			// 
+			// textBox2
+			// 
+			this.textBox2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+			this.textBox2.Font = new System.Drawing.Font("맑은 고딕", 9F);
+			this.textBox2.Location = new System.Drawing.Point(914, 487);
+			this.textBox2.MaxLength = 4;
+			this.textBox2.Name = "textBox2";
+			this.textBox2.Size = new System.Drawing.Size(77, 23);
+			this.textBox2.TabIndex = 10;
+			this.textBox2.Text = "120";
+			this.textBox2.TextChanged += new System.EventHandler(this.textBox_TextChanged);
+			this.textBox2.KeyDown += new System.Windows.Forms.KeyEventHandler(this.textBox2_KeyDown);
+			this.textBox2.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox2_KeyPress);
+			// 
+			// saveFileDialog1
+			// 
+			this.saveFileDialog1.Filter = "그림 파일|*.jpg";
+			// 
+			// groupBox1
+			// 
+			this.groupBox1.Location = new System.Drawing.Point(820, 86);
+			this.groupBox1.Name = "groupBox1";
+			this.groupBox1.Size = new System.Drawing.Size(189, 349);
+			this.groupBox1.TabIndex = 19;
+			this.groupBox1.TabStop = false;
+			this.groupBox1.Text = "해상도 목록";
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Font = new System.Drawing.Font("맑은 고딕", 9F);
+			this.label1.Location = new System.Drawing.Point(818, 52);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(11, 15);
+			this.label1.TabIndex = 20;
+			this.label1.Text = " ";
+			// 
+			// button2
+			// 
+			this.button2.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+			this.button2.Location = new System.Drawing.Point(3116, 57);
+			this.button2.Name = "button2";
+			this.button2.Size = new System.Drawing.Size(75, 23);
+			this.button2.TabIndex = 21;
+			this.button2.TabStop = false;
+			this.button2.Text = "button2";
+			this.button2.UseVisualStyleBackColor = true;
+			this.button2.Click += new System.EventHandler(this.button2_Click);
+			// 
+			// Form1
+			// 
+			this.AllowDrop = true;
+			this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.CancelButton = this.button2;
+			this.ClientSize = new System.Drawing.Size(1021, 520);
+			this.Controls.Add(this.button2);
+			this.Controls.Add(this.label1);
+			this.Controls.Add(this.groupBox1);
+			this.Controls.Add(this.textBox2);
+			this.Controls.Add(this.textBox1);
+			this.Controls.Add(this.label7);
+			this.Controls.Add(this.label6);
+			this.Controls.Add(this.label5);
+			this.Controls.Add(this.button1);
+			this.Controls.Add(this.pictureBox1);
+			this.DoubleBuffered = true;
+			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+			this.KeyPreview = true;
+			this.MaximizeBox = false;
+			this.Name = "Form1";
+			this.Text = "그림 자르기";
+			this.Load += new System.EventHandler(this.Form1_Load);
+			this.DragDrop += new System.Windows.Forms.DragEventHandler(this.Form1_DragDrop);
+			this.DragEnter += new System.Windows.Forms.DragEventHandler(this.Form1_DragEnter);
+			this.PreviewKeyDown += new System.Windows.Forms.PreviewKeyDownEventHandler(this.Form1_PreviewKeyDown);
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.PictureBox pictureBox1;
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.OpenFileDialog openFileDialog1;
+		private System.Windows.Forms.Label label5;
+		private System.Windows.Forms.Label label6;
+		private System.Windows.Forms.Label label7;
+		private System.Windows.Forms.TextBox textBox1;
+		private System.Windows.Forms.TextBox textBox2;
+		private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+		private System.Windows.Forms.GroupBox groupBox1;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.Button button2;
+	}
+}
+
---
+++ BackCropper/Form1.cs
@@ -0,0 +1,356 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Drawing.Drawing2D;
+using System.Drawing.Imaging;
+using System.IO;
+
+namespace BackCropper {
+	public partial class Form1 : Form {
+		public Form1() {
+			InitializeComponent();
+			this.MouseWheel += new MouseEventHandler(Mouse_Wheel);
+		}
+		private void textBox1_KeyPress(object sender, KeyPressEventArgs e) {
+			if (!(char.IsDigit(e.KeyChar) || e.KeyChar == Convert.ToChar(Keys.Back))) {
+				e.Handled = true;
+			}
+		}
+		private void textBox2_KeyPress(object sender, KeyPressEventArgs e) {
+			if (!(char.IsDigit(e.KeyChar) || e.KeyChar == Convert.ToChar(Keys.Back))) {
+				e.Handled = true;
+			}
+		}
+		double size, RectResol;
+		Point Resol;
+		Size nowSize, RectSize, SaveSize;
+		Image original, resized;
+		Point startPoint;
+		private int x = 0;
+		private int y = 0;
+		double Multiple;
+		bool isLoaded = false;
+		bool saving = false;
+		string fname;
+
+		public Bitmap ResizeImage(Image img, int resizedW, int resizedH) {
+			Bitmap bmp = new Bitmap(resizedW, resizedH);
+			Graphics graphic = Graphics.FromImage((Image)bmp);
+			graphic.InterpolationMode = InterpolationMode.HighQualityBicubic;
+			graphic.DrawImage(img, 0, 0, resizedW, resizedH);
+			graphic.Dispose();
+			return bmp;
+		}
+
+		private void Mouse_Wheel(object sender, MouseEventArgs e) {
+			if (!isLoaded) { return; }
+			Size tResult;
+
+			if ((e.Delta / 120) > 0) {
+				tResult = new Size(RectSize.Width + 10, (RectSize.Width + 10) * SaveSize.Height / SaveSize.Width);
+				if (tResult.Width > nowSize.Width || tResult.Height > nowSize.Height) {
+					if (tResult.Width > nowSize.Width) {
+						tResult = new Size(nowSize.Width, (nowSize.Width) * SaveSize.Height / SaveSize.Width);
+					} else {
+						tResult = new Size((nowSize.Height) * SaveSize.Width / SaveSize.Height, nowSize.Height);
+					}
+				}
+				RectSize = tResult;
+				this.Text = original.Width + " * " + original.Height + " : " + RectSize.Width + " * " + RectSize.Height;
+			} else {
+				tResult = new Size(RectSize.Width - 10, (RectSize.Width - 10) * SaveSize.Height / SaveSize.Width);
+				if (tResult.Width <= 9 || tResult.Height <= 9) { return; }
+				RectSize = tResult;
+				this.Text = original.Width + " * " + original.Height + " : " + RectSize.Width + " * " + RectSize.Height;
+			}
+			pictureBox1.Invalidate();
+		}
+
+		private void makePicture() {
+			label1.Text = this.Text = original.Width + " * " + original.Height;
+			isLoaded = true; saving = false;
+			this.Activate();
+			this.Focus();
+			try {
+				int Width = original.Width;
+				int Height = original.Height;
+				double temp;
+				double WperH = (double)Width / (double)Height;
+
+				if (WperH > size) {
+					temp = (double)Resol.X * (double)Height / (double)Width;
+					resized = ResizeImage(original, Resol.X, (int)temp);
+					nowSize = pictureBox1.Size = new Size(Resol.X, (int)temp);
+				} else {
+					temp = (double)Resol.Y * (double)Width / (double)Height;
+					resized = ResizeImage(original, (int)temp, Resol.Y);
+					nowSize = pictureBox1.Size = new Size((int)temp, Resol.Y);
+				}
+				pictureBox1.Image = resized;
+				Multiple = (double)original.Width / (double)nowSize.Width;
+
+				//MessageBox.Show(Resol[index].X + "\n" + Resol[index].Y + "\n" + size[index] + "\n" + temp);
+				fullSelect();
+				pictureBox1.Invalidate();
+			} catch {
+				// Resize Error...
+			}
+		}
+
+		private bool isRealPicture() {
+			try {
+				StreamReader sr = new StreamReader(fname);
+				original = Image.FromStream(sr.BaseStream);
+				sr.Close();
+			} catch {
+				MessageBox.Show("Not Available Picture", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+				return false;
+			}
+			return true;
+		}
+
+
+		string filename;
+		string[] splitstr;
+
+		private void button1_Click(object sender, EventArgs e) {
+			if (openFileDialog1.ShowDialog() != DialogResult.OK) { return; }
+			fname = openFileDialog1.FileName;
+			splitstr = fname.Split('\\');
+			splitstr = splitstr[splitstr.Length - 1].Split('.');
+			filename = "";
+			for (int i = 0; i < splitstr.Length - 1; i++) {
+				filename += splitstr[i];
+				if (i == splitstr.Length - 2) { break; }
+				filename += ".";
+			}
+			saveFileDialog1.FileName = filename;
+			if (isRealPicture()) { makePicture(); }
+		}
+
+		Label[] lbList;
+		int[] cutWidth, cutHeight;
+		string[] cutList;
+
+		private void Form1_Load(object sender, EventArgs e) {
+			this.Location = new Point(Screen.PrimaryScreen.WorkingArea.Right - 1050, Screen.PrimaryScreen.WorkingArea.Top + 100);
+			original = pictureBox1.Image;
+
+			size = 1.6;
+			Resol = new Point(800, 500);
+			nowSize = new Size(800, 500);
+
+			RectSize = new Size(Convert.ToInt32(textBox1.Text), Convert.ToInt32(textBox2.Text));
+			SaveSize = RectSize;
+			RectResol = 0.56666666666666666666666666666667;
+			saving = true;
+
+			pictureBox1.Cursor = Cursors.Cross;
+
+			cutList = new string[] { "캐릭터 사진 (100*120)", "애니메이션 커버 (375*148)", "모바일 배경 (272*480)", "프로필 사진 (320*320)", "아이폰 배경 (320*480)", "모바일 배경 (480*800)", "아이폰 배경 (640*960)", "모바일 배경 (720*1280)", "배경화면 (1280*720)", "배경화면 (1366*768)" };
+			cutWidth = new int[] { 100, 375, 272, 320, 320, 480, 640, 720, 1280, 1366 };
+			cutHeight = new int[] { 120, 148, 480, 320, 480, 800, 960, 1280, 720, 768 };
+			lbList = new Label[cutList.Count()];
+			for (int i = 0; i < cutList.Length; i++) {
+				lbList[i] = new Label();
+				lbList[i].Name = "lbList_" + i;
+				lbList[i].Font = new Font("맑은 고딕", 9);
+				lbList[i].Size = new Size(170, 23);
+				lbList[i].Location = new Point(10, i * 30 + 25);
+				lbList[i].Text = cutList[i];
+				lbList[i].MouseEnter += new EventHandler(lbList_MouseEnter);
+				lbList[i].MouseLeave += new EventHandler(lbList_MouseLeave);
+				lbList[i].MouseClick += new MouseEventHandler(lbList_MouseClick);
+				lbList[i].Cursor = Cursors.Hand;
+				groupBox1.Controls.Add(lbList[i]);
+			}
+		}
+
+		void lbList_MouseClick(object sender, MouseEventArgs e) {
+			int idx = Convert.ToInt32(((Label)sender).Name.Split('_')[1]);
+			textBox1.Text = cutWidth[idx].ToString();
+			textBox2.Text = cutHeight[idx].ToString();
+		}
+
+		void lbList_MouseLeave(object sender, EventArgs e) {
+			((Label)sender).ForeColor = Color.Black;
+			((Label)sender).Font = new Font("맑은 고딕", 9, FontStyle.Regular);
+		}
+
+		void lbList_MouseEnter(object sender, EventArgs e) {
+			((Label)sender).ForeColor = Color.OrangeRed;
+			((Label)sender).Font = new Font("맑은 고딕", 9, FontStyle.Underline);
+		}
+
+		private void pictureBox1_Paint(object sender, PaintEventArgs e) {
+			if (saving) { return; }
+			Pen skyBluePen = new Pen(Brushes.DeepSkyBlue);
+
+			int sX = x - RectSize.Width / 2, sY = y - RectSize.Height / 2;
+
+			if (sX < 0) { sX = 0; }
+			if (sX + RectSize.Width > nowSize.Width) { sX = nowSize.Width - RectSize.Width; }
+			if (sY < 0) { sY = 0; }
+			if (sY + RectSize.Height > nowSize.Height) { sY = nowSize.Height - RectSize.Height; }
+
+			Brush brush = new SolidBrush(Color.FromArgb(160, 0, 162, 232));
+			e.Graphics.FillRectangle(brush, sX, sY, RectSize.Width, RectSize.Height);
+			//e.Graphics.DrawRectangle(skyBluePen, sX, sY, RectSize.Width - 1, RectSize.Height - 1);
+			startPoint = new Point(sX, sY);
+		}
+
+		private ImageCodecInfo getEncoderInfo(string mimeType) {
+			// Get image codecs for all image formats 
+			ImageCodecInfo[] codecs = ImageCodecInfo.GetImageEncoders();
+			// Find the correct image codec 
+			for (int i = 0; i < codecs.Length; i++) {
+				if (codecs[i].MimeType == mimeType) { return codecs[i]; }
+			}
+			return null;
+		}
+
+		private void saveJpeg(string path, Bitmap img, long quality) {
+			// Encoder parameter for image quality
+
+			EncoderParameter qualityParam = new EncoderParameter(System.Drawing.Imaging.Encoder.Quality, quality);
+			// Jpeg image codec 
+			ImageCodecInfo jpegCodec = this.getEncoderInfo("image/jpeg");
+			if (jpegCodec == null) { return; }
+			EncoderParameters encoderParams = new EncoderParameters(1);
+			encoderParams.Param[0] = qualityParam;
+			img.Save(path, jpegCodec, encoderParams);
+		}
+
+		private void fullSelect() {
+			double temp;
+			if (RectResol > (double)nowSize.Width / (double)nowSize.Height) {
+				temp = (double)nowSize.Width * (double)SaveSize.Height / (double)SaveSize.Width;
+				RectSize = new Size(nowSize.Width, (int)temp);
+			} else {
+				temp = (double)nowSize.Height * (double)SaveSize.Width / (double)SaveSize.Height;
+				RectSize = new Size((int)temp, nowSize.Height);
+			}
+			pictureBox1.Invalidate();
+		}
+
+		private void textBox_TextChanged(object sender, EventArgs e) {
+			try {
+				RectSize = SaveSize = new Size(Convert.ToInt32(textBox1.Text), Convert.ToInt32(textBox2.Text));
+				RectResol = (double)RectSize.Width / (double)RectSize.Height;
+				fullSelect();
+			} catch {
+			}
+
+			//MessageBox.Show("Text Changed");
+		}
+
+		private void pictureBox1_MouseMove(object sender, MouseEventArgs e) {
+			x = e.X; y = e.Y;
+			pictureBox1.Invalidate();
+		}
+
+		Bitmap save;
+		private void pictureBox1_Click(object sender, EventArgs e) {
+			if (!isLoaded) {
+				button1_Click(null, null);
+				return;
+			}
+			double tStartX, tStartY, tCropWidth, tCropHeight;
+			int StartX, StartY, CropWidth, CropHeight;
+
+			tStartX = (double)startPoint.X * Multiple;
+			tStartY = (double)startPoint.Y * Multiple;
+			tCropWidth = (double)RectSize.Width * Multiple;
+			tCropHeight = (double)RectSize.Height * Multiple;
+			StartX = (int)tStartX;
+			StartY = (int)tStartY;
+			CropWidth = (int)tCropWidth;
+			CropHeight = (int)tCropHeight;
+
+			//MessageBox.Show(CropWidth.ToString() + "\n" + CropHeight.ToString());
+
+			Rectangle rect = new Rectangle(StartX, StartY, CropWidth, CropHeight);
+			Bitmap OriginalImage = new Bitmap(original, original.Width, original.Height);
+			Bitmap img = new Bitmap(CropWidth, CropHeight);
+			Graphics g = Graphics.FromImage(img);
+			g.InterpolationMode = InterpolationMode.HighQualityBicubic;
+			g.PixelOffsetMode = PixelOffsetMode.HighQuality;
+			g.CompositingQuality = CompositingQuality.HighQuality;
+			g.DrawImage(OriginalImage, 0, 0, rect, GraphicsUnit.Pixel);
+
+			if (textBox1.Text == "375" && textBox2.Text == "148") {
+				//g.FillRectangle(new SolidBrush(Color.FromArgb(120, 255, 255, 255)), 0, 0, CropWidth, CropHeight);
+			}
+
+
+			save = ResizeImage(img, SaveSize.Width, SaveSize.Height);
+			Image PreView;
+
+			double temp;
+			double WperH = (double)SaveSize.Width / (double)SaveSize.Height;
+
+			if (WperH > (double)nowSize.Width / (double)nowSize.Height) {
+				temp = (double)nowSize.Width * (double)SaveSize.Height / (double)SaveSize.Width;
+				PreView = ResizeImage(save, nowSize.Width, (int)temp);
+			} else {
+				temp = (double)nowSize.Height * (double)SaveSize.Width / (double)SaveSize.Height;
+				PreView = ResizeImage(save, (int)temp, nowSize.Height);
+			}
+
+			pictureBox1.Image = PreView;
+			saving = true;
+			pictureBox1.Invalidate();
+
+			Timer saveTimer = new Timer();
+			saveTimer.Interval = 1500; saveTimer.Tick += saveTimer_Tick;
+			saveTimer.Start();
+
+			//pictureBox1.Width = PreView.Width;
+			//pictureBox1.Height = PreView.Height;
+
+			//MessageBox.Show(startPoint.X.ToString() + "\n" + startPoint.Y.ToString() + "\n" + Multiple.ToString());
+		}
+
+		void saveTimer_Tick(object sender, EventArgs e) {
+			((Timer)sender).Stop();
+			saveFileDialog1.FileName = filename;
+			//MessageBox.Show(filename);
+			if (saveFileDialog1.ShowDialog() != DialogResult.OK) {
+				pictureBox1.Image = resized; saving = false;
+				return;
+			}
+			//Image PreView = ResizeImage(img, RectSize.Width, RectSize.Height);
+
+			saveJpeg(saveFileDialog1.FileName, save, 100);
+
+			//save.Save(saveFileDialog1.FileName);
+			pictureBox1.Image = resized;
+			saving = false;
+		}
+
+		private void Form1_DragDrop(object sender, DragEventArgs e) {
+			var directoryName = (string[])e.Data.GetData(DataFormats.FileDrop);
+			fname = directoryName[0];
+			splitstr = fname.Split('\\');
+			splitstr = splitstr[splitstr.Length - 1].Split('.');
+			filename = "";
+			for (int i = 0; i < splitstr.Length - 1; i++) {
+				filename += splitstr[i];
+				if (i == splitstr.Length - 2) { break; }
+				filename += ".";
+			}
+			saveFileDialog1.FileName = filename;
+			if (isRealPicture()) { makePicture(); }
+		}
+		private void Form1_DragEnter(object sender, DragEventArgs e) { e.Effect = e.Data.GetDataPresent(DataFormats.FileDrop) ? DragDropEffects.Copy : DragDropEffects.None; }
+		private void Form1_PreviewKeyDown(object sender, PreviewKeyDownEventArgs e) { label1.Text = e.Control.ToString(); if (e.Control && e.KeyCode == Keys.O) { button1_Click(null, null); } }
+		private void textBox1_KeyDown(object sender, KeyEventArgs e) { if (e.Control && e.KeyCode == Keys.O) { button1_Click(null, null); } }
+		private void textBox2_KeyDown(object sender, KeyEventArgs e) { if (e.Control && e.KeyCode == Keys.O) { button1_Click(null, null); } }
+		private void button2_Click(object sender, EventArgs e) { Application.Exit(); }
+	}
+}
